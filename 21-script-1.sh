89a5541660e3ff6efc81c17d4d3cc505a4a91a67
Manasa <manasa.vosur@gmail.com>
2026-02-03 16:57:18 +0530

    actions

--- 21-script-1.sh
+++ 21-script-1.sh
@@ -6,4 +6,19 @@ echo "I am in: $COUNTRY"
 echo "PID of SCRIPT-1: $$"
 
 # sh 22-script-2.sh    #PID will be different for script-1 & script-2 if this command is run
+
+# example: output for sh command-
+# I am in: INDIA
+# PID of SCRIPT-1: 4157
+# I am in:
+# PID of SCRIPT-2: 4158
+
+
 source ./22-script-2.sh     #PID will be same for script-1 & script-2 if this command is run
+
+# example: output for source command-
+
+# I am in: INDIA
+# PID of SCRIPT-1: 4286
+# I am in: INDIA
+# PID of SCRIPT-2: 4286
